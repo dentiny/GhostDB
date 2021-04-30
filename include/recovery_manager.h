baafc5d947873a2f092f4d4166fe8b9e2f039b52
dentiny <1910821171@qq.com>
2021-04-30 17:33:14 -0400

    RecoveryManager

--- include/recovery_manager.h
+++ include/recovery_manager.h
@@ -13,14 +13,18 @@
 #pragma once
 
 #include "common.h"
+#include "compaction_manager.h"
 #include "config.h"
 #include "disk_manager.h"
+#include "sstable_manager.h"
 
 namespace ghostdb {
 
 class RecoveryManager {
  public:
-  RecoveryManager(DiskManager *disk_manager);
+  RecoveryManager(DiskManager *disk_manager,
+                  SSTableManager *sstable_manager,
+                  CompactionManager *compaction_manager);
   ~RecoveryManager() = default;
   void PerformRecover();
 
@@ -29,7 +33,12 @@ class RecoveryManager {
   void RecoverWAL();
 
  private:
+  /** directly read and write files on disk */
   DiskManager *disk_manager_;
+  /** dump WAL into the SSTable file */
+  SSTableManager *sstable_manager_;
+  /** dumping WAL may need minor compaction */
+  CompactionManager *compaction_manager_;
 };
 
 }  // namespace ghostdb
--- src/recovery_manager.cc
+++ src/recovery_manager.cc
@@ -17,15 +17,19 @@
 #include "util.h"
 namespace ghostdb {
 
-RecoveryManager::RecoveryManager(DiskManager *disk_manager) :
-  disk_manager_(disk_manager) {}
+RecoveryManager::RecoveryManager(DiskManager *disk_manager,
+                                  SSTableManager *sstable_manager,
+                                  CompactionManager *compaction_manager) :
+  disk_manager_(disk_manager),
+  sstable_manager_(sstable_manager),
+  compaction_manager_(compaction_manager) {}
 
 void RecoveryManager::PerformRecover() {
   RecoverSSTable();
   RecoverWAL();
 }
 
-// TODO: currently cannot 
+// TODO: currently don't support multiple pages
 void RecoveryManager::RecoverSSTable() {
   SSTablePage sstable_page;
   disk_manager_->ReadDb(sstable_page.data_);
@@ -36,6 +40,7 @@ void RecoveryManager::RecoverSSTable() {
   LOG_DEBUG("need to recovery SSTable file");
   int page_id = sstable_page.GetPageId();
   disk_manager_->WriteDb(sstable_page.data_, page_id);
+  sstable_manager_->ClearTempSSTable();
 }
 
 // WAL is guarenteed to be within PAGE_SIZE.
@@ -48,19 +53,38 @@ void RecoveryManager::RecoverWAL() {
   }
 
   // Keep reading key-value pairs, until two <0, 0> is met.
-  // LOG_DEBUG("need to recover WAL file");
-  // bool has_met_zero = false;
-  // size_t offset = 0;
-  // Key key;
-  // Val val;
-  // while (true) {
-  //   memmove(&key, page_data, sizeof(key));
-  //   offset += sizeof(key);
-  //   memmove(&val, page_data, sizeof(val));
-  //   offset += sizeof(val);
+  LOG_DEBUG("need to recover WAL file");
+  bool has_met_zero = false;
+  size_t offset = 0;
+  Key key;
+  Val val;
+  buffer_t buffer;
+  while (true) {
+    memmove(&key, page_data, sizeof(key));
+    offset += sizeof(key);
+    memmove(&val, page_data, sizeof(val));
+    offset += sizeof(val);
 
-  //   // TODO
-  // }
+    // Check whether has met the end of WAL.
+    if (key == 0 && val == 0) {
+      if (has_met_zero) {
+        break;
+      } else {
+        has_met_zero = true;
+      }
+    } else if (has_met_zero) {
+      buffer[0] = 0;
+      has_met_zero = false;
+    }
+
+    buffer[key] = val;
+  }
+
+  // Dump key-value pairs to SSTable file, and clear WAL file.
+  while (!sstable_manager_->DumpSSTable(buffer)) {
+    compaction_manager_->RequestMinorCompaction();
+  }
+  disk_manager_->WriteLog(zero_data, PAGE_SIZE, true /* is_delete */);
 }
 
 }  // namespace ghostdb
